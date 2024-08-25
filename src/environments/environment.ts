a42cdea97afd2b8a236ebd8cf5c7f9fefe8e3199
Rahul Macha <79196928+rahulmacha65@users.noreply.github.com>
2024-08-25 19:02:47 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  
+  firebase : {
+    apiKey: "AIzaSyBbjBLXWIRMZAjXbNKrbIDQKRYvWo8fq8U",
+    authDomain: "moderndictionary-5869f.firebaseapp.com",
+    databaseURL: "https://moderndictionary-5869f-default-rtdb.firebaseio.com",
+    projectId: "moderndictionary-5869f",
+    storageBucket: "moderndictionary-5869f.appspot.com",
+    messagingSenderId: "746813368934",
+    appId: "1:746813368934:web:bd121f6805d9cc62aedba7"
+  }
 };
 
 /*
